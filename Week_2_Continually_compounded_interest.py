31f75dac8e2e724ca14b3aa1d9b77d5a39899124
Isaac Martin <45830500+isaacmartin1@users.noreply.github.com>
2021-02-03 17:00:05 -0600

    Add files via upload

--- Week_2_Continually_compounded_interest.py
+++ Week_2_Continually_compounded_interest.py
@@ -1,76 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[36]:
 
 
 import matplotlib.pyplot as plt
 import numpy as np
-import math
-
-
-# time only doing 50 years
-t = range(50)
-#intrest rate
-r = .02
-# number of coumpunds per year
-n = 1
-
-## just graphing stuff
-y = [];
-for time in t:
-    #we add the amount of money after x years to a list so it can be graphed on teh y axis vs time
-    y.append(math.pow(1*(1+(r/n)),n*time))
-
-# setting the axes at the centre
-
-# plot the function
-plt.plot(t,y, 'g')
-plt.title("stonks")
-plt.xlabel("years")
-plt.ylabel("that Money")
-
-
-# In[46]:
-
-
-
-def difftime(number):
-    t = range(50) 
-    #intrest rate
-    r = .3
-    # number of coumpunds per year
-    n = number
-    print(n)
-    ## just graphing stuff
-    y = [];
-    for time in t:
-        #we add the amount of money after x years to a list so it can be graphed on teh y axis vs time
-        y.append(math.pow(1*(1+(r/n)),n*time))
-    plt.plot(t,y)
 
 values = [1,12,365]
+t = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+r = 1
 
 for x in values:
-    difftime(x)
-# plot the function
+    n = x
+    y = []
+    for t in time:
+        y.append((1+r/n)**(n*t))
+    plt.plot(time,y)
 
-plt.title("stonks")
-plt.xlabel("years")
-plt.ylabel("that Money")
+#list_of_e = []
+#for t in time:
+#    list_of_e.append(np.exp(r*t))
+#plt.plot(time, list_of_e)
+
+plt.title("Compound Interest on Initial Deposit")
+plt.xlabel("Time (Years)")
+plt.ylabel("$'s'")
 
 
-# In[50]:
+# In[35]:
 
 
-high = [10,1000,10000]
-for x in high:
-    difftime(x)
 
-t = range(50)    
-y = []
-for time in t:
-    #we add the amount of money after x years to a list so it can be graphed on teh y axis vs time
-    y.append(np.exp(.3 * time))
-plt.plot(t,y)
+
+
+# In[12]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
